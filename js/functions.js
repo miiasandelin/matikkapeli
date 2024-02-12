462df1048a72c57a4bfc481d03e5e9d78cf7823b
miiasandelin <n3sami00@students.oamk.fi>
2024-02-12 20:38:26 +0200

    Matikkapeli tehty

---
+++ js/functions.js
@@ -0,0 +1,55 @@
+let rand_num1 = 0
+let rand_num2 = 0
+let correct = 0
+let incorrect = 0
+
+const getRandomIntNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomIntNumberInRange(1,10)
+    rand_num2 = getRandomIntNumberInRange(1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+const incrementScore = (result) => {
+    if (result == true) {
+        correct++
+        document.querySelector('#correct').innerHTML = correct
+    } else {
+        incorrect++
+        document.querySelector('#incorrect').innerHTML = incorrect
+    }
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+});
+
+document.querySelector('button').addEventListener('click',() => {
+    handleSubmit()
+})
+
+function submitForm(event){
+    event.preventDefault();
+    handleSubmit()
+ }
+
+document.querySelector('#formId').addEventListener('submit', submitForm);
+
+const handleSubmit = () => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        incrementScore(true)
+        alert('Correct!')
+    } else {
+        incrementScore(false)
+        alert('Incorrect!')
+    }
+
+    randomizeNumbers()
+    document.querySelector('input').value=''
+}
